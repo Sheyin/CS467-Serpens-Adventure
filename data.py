efdf84819e5928bedb00eb52571db36ef6aeaa94
Matthew Hillman <hillmanm@flip3.engr.oregonstate.edu>
2017-04-17 05:46:55 -0700

    add data file

---
+++ data.py
@@ -0,0 +1,13 @@
+# Data Function
+# File which handles engine control and game initiation 
+
+# CS 467
+# Capstone Project 
+# Text Loading and software parsing - Matt Hillman 
+
+#[BEGIN IMPORTS]
+import os
+import json
+#[END IMPORTS]
+
+print "hello DATA"
