c6f36862e773573a7b13ee15dc065294e97e6dc5
Sheyin <sheyinkage@gmail.com>
2017-05-04 14:03:47 -0500

    Returns list of features/descriptions

--- utils.py
+++ utils.py
@@ -1,19 +1,42 @@
 import re
 
-# This should probably receive a filename or room name as input
+# These should probably receive a filename or room name as input
 # then buffer = parameter name (file/room name)
-def getItemList():
-	buffer = open('data/rooms/room3.json')
+def getFeaturesList():
+	file = open('data/rooms/room3.json')
+	featuresList = []
 
-	for line in buffer:
+	for line in file:
 		line = line.rstrip()
-		listOfFeatures = re.findall('"feat\d*": ("\D*")', line)
-		for i in range (0, len(listOfFeatures)):
-			if listOfFeatures[i].startswith('"') and listOfFeatures[i].endswith('"'):
-				listOfFeatures[i] = listOfFeatures[i][1:-1]
-				print listOfFeatures[i]
+		results = re.findall('"feat\d*": ("\D*")', line)
+		for i in range (0, len(results)):
+			if results[i].startswith('"') and results[i].endswith('"'):
+				results[i] = results[i][1:-1]
+				featuresList.append([results[i]])
+	file.close()
+	return featuresList
+
+
+def getDescriptionsList():
+	file = open('data/rooms/room3.json')
+	descriptionsList = []
+
+	for line in file:
+		line = line.rstrip()
+		results = re.findall('"feat\d*desc": ("\D*")', line)
+		for i in range (0, len(results)):
+			if results[i].startswith('"') and results[i].endswith('"'):
+				results[i] = results[i][1:-1]
+				descriptionsList.append([results[i]])
+	file.close()
+	return descriptionsList
 
 # Reference: Python for Informatics (http://www.py4inf.com/)
 # http://stackoverflow.com/questions/3085382/python-how-can-i-strip-first-and-last-double-quotes
 
-getItemList();
+# Below is just for my testing purposes
+if __name__ == "__main__":
+	featuresList = getFeaturesList();
+	descriptionsList = getDescriptionsList();
+	print "features: " + str(featuresList)
+	print "descriptions: " + str(descriptionsList)
