f09f0df7b1d828566836e898c811af13ea14d266
Sheyin <silveryn@gmail.com>
2017-05-11 20:51:06 -0500

    Update to create feature dictionary

--- utils.py
+++ utils.py
@@ -2,8 +2,45 @@ import re
 
 # These should probably receive a filename or room name as input
 # then buffer = parameter name (file/room name)
+# Python-3-safe way of getting list of keys: k = list(b.keys())
+# Python-2 way of getting list of keys: k = b.keys()
+def getFeaturesDict(currentRoom):
+	filename = 'data/rooms/room' + str(currentRoom) + '.json'
+	file = open(filename)
+	featuresList = []
+	featuresDict = {}
+	actionsList = []
+
+	for line in file:
+		line = line.rstrip()
+		results = re.findall('"feat\d*": ("\D*"),', line)
+		actions = re.findall('"feat\d*interactOptions": ("\D*"),', line)
+		
+		if (results != ""):
+			for i in range (0, len(results)):
+				# This will strip opening and closing parenthesis
+				if results[i].startswith('"') and results[i].endswith('"'):
+					results[i] = results[i][1:-1]
+					featuresList.append(results[i])
+
+		if (actions != ""):
+			for j in range (0, len(actions)):
+				# This will strip opening and closing parenthesis
+				if actions[j].startswith('"') and actions[j].endswith('"'):
+					actions[j] = actions[j][1:-1]
+					actionsList.append((actions[j]))
+					
+	file.close()
+
+	# Concatenate into a dictionary
+	for itemPos in range (0, len(featuresList)):
+		featuresDict[featuresList[itemPos]] =  actionsList[itemPos]
+
+	return featuresDict
+
+
+	''' # Working template
 def getFeaturesList(currentRoom):
-	#currentRoom = 3
 	filename = 'data/rooms/room' + str(currentRoom) + '.json'
 	file = open(filename)
 	featuresList = []
@@ -11,7 +48,6 @@ def getFeaturesList(currentRoom):
 	for line in file:
 		line = line.rstrip()
 		results = re.findall('"feat\d*": ("\D*"),', line)
-		#results = re.findall('"feat\d*": *', line)
 		
 		for i in range (0, len(results)):
 			# This will strip opening and closing parenthesis
@@ -20,22 +56,56 @@ def getFeaturesList(currentRoom):
 				featuresList.append(results[i])
 	file.close()
 	return featuresList
+	'''
 
 
-def getDescriptionsList(currentRoom):
+# When supplied a room number, it picks out the appropriate connections
+# and returns it in a list.  Ordering is significant.
+# 0: north, 1: south, 2: west, 3: east, 4: up, 5: down
+def getRoomConnections(currentRoom):
 	filename = 'data/rooms/room' + str(currentRoom) + '.json'
 	file = open(filename)
-	descriptionsList = []
+	connectionsList = []
+
+	# This appends the room number to the list, but we need to translate
+	# each room number into it's common name(s).  Also case sensitivity.
 
 	for line in file:
 		line = line.rstrip()
-		results = re.findall('"feat\d*desc": ("\D*")', line)
+		results = re.findall('"north": (\d*)', line)
 		for i in range (0, len(results)):
 			if results[i].startswith('"') and results[i].endswith('"'):
 				results[i] = results[i][1:-1]
-				descriptionsList.append(results[i])
+			connectionsList.append(('north', results[0]))
+		results = re.findall('"south": (\d*)', line)
+		for i in range (0, len(results)):
+			if results[i].startswith('"') and results[i].endswith('"'):
+				results[i] = results[i][1:-1]
+			connectionsList.append(('south', results[0]))
+		results = re.findall('"west": (\d*)', line)
+		for i in range (0, len(results)):
+			if results[i].startswith('"') and results[i].endswith('"'):
+				results[i] = results[i][1:-1]
+			connectionsList.append(('west', results[0]))
+		results = re.findall('"east": (\d*)', line)	
+		for i in range (0, len(results)):
+			if results[i].startswith('"') and results[i].endswith('"'):
+				results[i] = results[i][1:-1]
+			connectionsList.append(('east', results[0]))
+		results = re.findall('"up": (\d*)', line)
+		for i in range (0, len(results)):
+			if results[i].startswith('"') and results[i].endswith('"'):
+				results[i] = results[i][1:-1]
+			connectionsList.append(('up', results[0]))
+		results = re.findall('"down": (\d*)', line)
+		for i in range (0, len(results)):
+			if results[i].startswith('"') and results[i].endswith('"'):
+				results[i] = results[i][1:-1]
+			connectionsList.append(('down', results[0]))
+
 	file.close()
-	return descriptionsList
+	return connectionsList
+
 
 # Tries to translate input into a legal movement (direction)
 # Returns a cardinal direction
@@ -70,10 +140,13 @@ def getRoomInfo(currentRoom):
 
 # Reference: Python for Informatics (http://www.py4inf.com/)
 # http://stackoverflow.com/questions/3085382/python-how-can-i-strip-first-and-last-double-quotes
+# http://stackoverflow.com/questions/8953627/python-dictionary-keys-error
 
 # Below is just for my testing purposes
 if __name__ == "__main__":
-	featuresList = getFeaturesList();
-	descriptionsList = getDescriptionsList();
-	print "features: " + str(featuresList)
-	print "descriptions: " + str(descriptionsList)
+	featuresDict = getFeaturesDict(1);
+	connectionsList = getRoomConnections(1);
+	#descriptionsList = getDescriptionsList(1);
+	print "features: " + str(featuresDict)
+	print "connectionsList: " + str(connectionsList)
+	#print "descriptions: " + str(descriptionsList)
