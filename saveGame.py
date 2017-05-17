a6b44e5fb745d10346de545d9e02dab24b1ae615
Matthew Hillman <hillmanm@flip2.engr.oregonstate.edu>
2017-05-17 10:02:14 -0700

    update gamestate

--- saveGame.py
+++ saveGame.py
@@ -14,9 +14,9 @@ from gamestate import *
 currentState = MattsGameStateClass(1)
 	
 def load_gamestate (saveNum):
-	"This function loads data from each json file that is present in the /data/gamestate folder"
-	"For each json file, an ObjectClass object is instantiated and populated with data.  The "
-	"objects are then stored in a dictionary 'objects' that is keyed on the object name"
+	"This function loads data from a file named saveNum.json in the /data/gamestate folder"
+	"An object named currentState is instantiated and populated with the file data."
+
 
 	gs_list = os.listdir("data/gamestate")
 	
@@ -30,20 +30,34 @@ def load_gamestate (saveNum):
 			currentState.name = data["name"]
 			currentState.saveNum = saveNum
 			currentState.currRoom = data["currRoom"]
-			currentState.rm01vis = data["rm01vis"]
-			currentState.rm02vis = data["rm02vis"]
-			currentState.obj1Loc = data["obj1Loc"]
-			currentState.obj2Loc = data["obj2Loc"]
-			currentState.rm01f1 = data["rm01f1"]
-			currentState.rm01f2 = data["rm01f2"]
+			# currentState.rm01vis = data["rm01vis"]
+			# currentState.rm02vis = data["rm02vis"]
+			# currentState.obj1Loc = data["obj1Loc"]
+			# currentState.obj2Loc = data["obj2Loc"]
+			# currentState.rm01f1 = data["rm01f1"]
+			# currentState.rm01f2 = data["rm01f2"]
 		
 
 #inside function test print
-	print currentState.rm01f2
+	print currentState.name
 
-load_gamestate("0")
+load_gamestate("1")
 
-#outside function test print
-print currentState.rm01f2
+#outside load_gamestate function test print
+#print currentState.rm01f2
 print currentState.saveNum
 
+def save_gamestate(saveNum):
+    """
+    Creates a new json file with data from currentState object 
+    """
+    jsonObject = dict()
+    jsonObject["name"] = currentState.name
+    jsonObject["saveNum"] = saveNum
+    jsonObject["currRoom"] = currentState.currRoom
+    file_content = json.dumps(jsonObject, sort_keys=True, indent=4, separators=(',', ': '))
+
+    with open("data/gamestate/" + saveNum + ".json", "w+") as json_data:
+        json_data.write(file_content)
+
+save_gamestate("1")
