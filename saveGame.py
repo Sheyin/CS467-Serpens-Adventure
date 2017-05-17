9168b5641fed3edfcb3a2bf6e0f5e8b09e14c5d3
Matthew Hillman <hillmanm@flip2.engr.oregonstate.edu>
2017-05-17 08:11:44 -0700

    update gamestate

--- saveGame.py
+++ saveGame.py
@@ -8,17 +8,13 @@
 #[BEGIN IMPORTS]
 import os
 import json
-from room import *
-from objectC import *
 from gamestate import *
 #[END IMPORTS]
 
-
-#objects = {}
-current_state = MattsGameStateClass(1)
+currentState = MattsGameStateClass(1)
 	
 def load_gamestate ():
-	"This function loads data from each json file that is present in the /data/objects folder"
+	"This function loads data from each json file that is present in the /data/gamestate folder"
 	"For each json file, an ObjectClass object is instantiated and populated with data.  The "
 	"objects are then stored in a dictionary 'objects' that is keyed on the object name"
 #	objects = {}
@@ -29,34 +25,24 @@ def load_gamestate ():
 	#open each file as json	
 		with open("data/gamestate/" + game) as json_data:
 	
-	#instantiate temporary ObjectClass object as populate with data
+	#instantiate gamestate object and populate with data
 			data = json.load(json_data)
-			global current_state
-			current_state = MattsGameStateClass(data["name"])
-			
-			
-			current_state.name = data["name"]
-			current_state.currRoom = data["currRoom"]
-			current_state.rm01vis = data["rm01vis"]
-			current_state.rm02vis = data["rm02vis"]
-			current_state.obj1Loc = data["obj1Loc"]
-			current_state.obj2Loc = data["obj2Loc"]
-			current_state.rm01f1 = data["rm01f1"]
-			current_state.rm01f2 = data["rm01f2"]
+			global currentState
+			currentState = MattsGameStateClass(data["name"])
+			currentState.name = data["name"]
+			currentState.currRoom = data["currRoom"]
+			currentState.rm01vis = data["rm01vis"]
+			currentState.rm02vis = data["rm02vis"]
+			currentState.obj1Loc = data["obj1Loc"]
+			currentState.obj2Loc = data["obj2Loc"]
+			currentState.rm01f1 = data["rm01f1"]
+			currentState.rm01f2 = data["rm01f2"]
 		
 
-	#add object to dictionary (object name is dictionary key)
-
-#			objects[current_object.name] = current_object
-
-	
-	#test prints for objects dictionary
-
-	print current_state.rm01f2
-	
-		
-			
-
+#inside function test print
+	print currentState.rm01f2
 
 load_gamestate()
-print current_state.rm01f2
+
+#outside function test print
+print currentState.rm01f2
